55fc11235a3d73933f22f29edc9181be0980264d
orenb99 <77238204+orenb99@users.noreply.github.com>
2021-05-15 02:44:01 +0300

    added image

--- src/components/Message.js
+++ src/components/Message.js
@@ -1,7 +1,12 @@
 import React from "react";
 
 function Message({ content, username, time, image }) {
-  return <div className="message">{`[${time}] ${username}: ${content}`}</div>;
+  return (
+    <div className="message">
+      <img src={image} className="image-icon" />
+      {`[${time}] ${username}: ${content}`}
+    </div>
+  );
 }
 
 export default Message;
